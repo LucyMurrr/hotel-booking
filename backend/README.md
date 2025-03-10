7361632b3a4c390c49c3f0b2eb43dbf5a8a92abf
Abikimoz <i.m.anisimov25@gmail.com>
2025-03-10 20:22:00 +0300

    add gen back

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,12 @@
 Для запуска использовать docker-compose up --build
 
 Линтер запускается командой ./gradlew checkstyleMain
+
+Для выполнения генерации из openAPI:
+1. Компиляция в openapi.yaml
+   npm install -g @typespec/compiler
+   tsp compile main.tsp --emit=@typespec/openapi3
+2. Скачиваю https://github.com/OpenAPITools/openapi-generator.git
+   wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar -O openapi-generator-cli.jar
+3. Запускаю генерацию
+   ./gradlew checkClasspath
--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,26 @@ repositories {
 	mavenCentral()
 }
 
+tasks.register<JavaExec>("generateServerCode") {
+	group = "openapi"
+	description = "Generate server code from OpenAPI specification"
+
+	// Укажите путь к JAR-файлу OpenAPI Generator
+	classpath = files("../api/openapi-generator-cli-7.12.0.jar")
+
+	// Основной класс для запуска
+	mainClass.set("org.openapitools.codegen.OpenAPIGenerator")
+
+	// Аргументы для OpenAPI Generator
+	args(
+		"generate",
+		"-i", "../api/tsp-output/schema/openapi.yaml", // Путь к вашему openapi.yaml
+		"-g", "spring",              // Генерация кода для Spring Boot
+		"-o", "../api/generated-code", // Выходная директория
+		"--additional-properties", "useSpringBoot3=true,dateLibrary=java8,library=spring-boot"
+	)
+}
+
 dependencies {
 	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation ("org.springframework.boot:spring-boot-starter-web")
