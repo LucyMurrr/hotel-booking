787feed8a4695b364e241fa67a46e0aa9c85dfc0
OzhoginCode <danozh.b@gmail.com>
2025-03-16 19:49:20 +0200

    fix eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,66 +8,65 @@ module.exports = {
       'eslint:recommended',
       'plugin:react/recommended',
       'plugin:react/jsx-runtime',
-      // 'plugin:react-hooks/recommended',
+      'plugin:react-hooks/recommended',
       'airbnb',
       'plugin:@typescript-eslint/strict-type-checked',
-      // 'plugin:@typescript-eslint/stylistic-type-checked',
-      'next/core-web-vitals',
-      'next/typescript',
+      // 'plugin:@typescript-eslint/stylistic-type-checked', // TODO: включить, когда будет готова основная кодовая база
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     parserOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       projectService: true,
-      project: './tsconfig.app.json',
+      project: './tsconfig.json',
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx']
+      },
+      'import/resolver': {
+        'typescript': {},
+      },
+      react: { version: '19' } },
     plugins: ['react-refresh', '@typescript-eslint'],
     rules: {
       'max-len': ['error', {'code': 108 }],
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
       'react/react-in-jsx-scope': 'off',
       'react/jsx-one-expression-per-line': 'off',
-      'react/prop-types': 'off',
       'import/extensions': 'off',
       'import/no-anonymous-default-export': 'off',
+      'import/no-unresolved': 'off', // FIX: если убрать это правило, то eslint начинает ругаться на TS алиасы (причём, возможно, только в VS Code). Надо пофиксить
       'object-curly-newline': ['error', {
         multiline: true,
         minProperties: 5,
         consistent: true,
       }],
-      "react/function-component-definition": [
-        "error",
+      'react/function-component-definition': [
+        'error',
         {
-          "namedComponents": "arrow-function",
-          "unnamedComponents": "arrow-function",
+          'namedComponents': 'arrow-function',
+          'unnamedComponents': 'arrow-function',
         }
       ],
       'jsx-a11y/label-has-associated-control': 'off',
-      'react/jsx-filename-extension': [0,
-          {
-            "extensions": [
-              ".tsx"
-            ]
-          }
-        ],
-        '@next/next/no-html-link-for-pages': ['error', './app'],
-        '@next/next/no-img-element': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-confusing-void-expression': 'off',
-        '@typescript-eslint/no-misused-promises': 'off',
-        '@typescript-eslint/unbound-method': 'off'
+      'react/jsx-filename-extension': [0, { 'extensions': ['.tsx'] }],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/unbound-method': 'off',
     },
     overrides: [
       {
         files: ['**/*.js', '**/*.jsx'],
         extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      },
+      {
+        files: ['./vite.config.ts', './app/routes.ts', './app/src/api/index.ts'],
+        rules: {
+          'import/no-extraneous-dependencies': 'off',
+        },
       },
     ]
   }
