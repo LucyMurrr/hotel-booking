cef01d68fd45e952e9d5f4ec1a8eecd004780c8a
Abikimoz <i.m.anisimov25@gmail.com>
2025-04-17 23:25:26 +0300

    run swagger and devStatus

--- backend/src/main/java/aim/hotel_booking/controller/TestController.java
+++ backend/src/main/java/aim/hotel_booking/controller/TestController.java
@@ -1,4 +1,4 @@
-package aim.hotel_booking.сontroller;
+package aim.hotel_booking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +10,4 @@ public class TestController {
     public String test() {
         return "Application is running!";
     }
-}
+} 
