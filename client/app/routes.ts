0c6126b8ded1986680175c2d68bb403f98e258b4
OzhoginCode <danozh.b@gmail.com>
2025-05-04 20:01:09 +0300

    refactor base layout

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -3,12 +3,10 @@ import { type RouteConfig, index, route, layout } from '@react-router/dev/routes
 export default [
   layout('routes/baseLayout.tsx', [
     index('routes/hotels.tsx'),
-    // route('/hotels/:hotelId/rooms', 'routes/room.tsx'),
     route('/hotels/:hotelId/rooms/:roomId', 'routes/room.tsx'),
-    route('/auth', 'routes/authorisation.form.tsx'),
+    route('/auth', 'routes/authorization.form.tsx'),
     route('/profile', 'routes/profile.tsx'),
     route('/hotels/:hotelId', 'routes/hotel.tsx'),
     route('/hotels/:hotelId/newRoom', 'routes/newRoom.Form.tsx'),
-
   ]),
 ] satisfies RouteConfig;
