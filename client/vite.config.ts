fe746c54ef324b901b33f647759bc790c1252476
Lusi Mur <lucymur.dev@gmail.com>
2025-05-14 03:53:31 +0300

    added tsconfig.dev.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,22 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  define: {
-    global: 'window',
-  },
-  server: {
-    host: process.env.HOST,
-  },
+export default defineConfig(({ mode }) => {
+  const tsConfigPath = mode === 'development' ? './tsconfig.dev.json' : './tsconfig.json';
+
+  return {
+    plugins: [
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths({
+        projects: [tsConfigPath],
+      }),
+    ],
+    define: {
+      global: 'window',
+    },
+    server: {
+      host: process.env.HOST || true,
+    },
+  };
 });
