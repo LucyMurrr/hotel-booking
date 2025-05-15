4f261315ce6ade53471cec57751e7e48a23d1120
OzhoginCode <danozh.b@gmail.com>
2025-05-15 19:31:18 +0300

    fix backend url

--- client/app/src/api/index.ts
+++ client/app/src/api/index.ts
@@ -1,7 +1,7 @@
 import { DefaultApi, Configuration } from '@generatedClient/index';
 
 const config = new Configuration({
-  basePath: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://localhost/api',
+  basePath: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://hexling.ru/api',
   fetchApi: async (url, init) => {
     const token = localStorage.getItem('token');
     const headers = new Headers(init?.headers || {});
