7b23bee5e6e3db48eecfb64ef1aebcdd848dcb1e
Lusi Mur <lucymur.dev@gmail.com>
2025-05-18 23:09:07 +0300

    pass?

--- tests/playwright/tests/support.spec.js
+++ tests/playwright/tests/support.spec.js
@@ -2,12 +2,10 @@ import { test, expect } from '../fixtures/auth.fixture';
 
 test('test', async ({ page }) => {
   const uniqueId = `${Date.now()}${Math.floor(Math.random() * 1000)}`;
-  console.log(uniqueId);
   await page.getByRole('button', { name: 'message' }).click();
   await page.getByRole('textbox', { name: 'Введите сообщение' }).click();
   await page.getByRole('textbox', { name: 'Введите сообщение' }).fill(`test message${uniqueId}`);
   await page.getByRole('button', { name: 'send' }).click();
-  console.log(`Сообщение отправлено test message${uniqueId}`);
   await expect(page.getByText(`test message${uniqueId}`)).toBeVisible({ timeout: 10_000 });
   await page.getByRole('button', { name: 'Close' }).click();
   await page.getByRole('button', { name: 'user Профиль' }).click();
