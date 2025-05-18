a3ecb8be0f8657c5f860cd05624c9a8d2130a1b3
Lusi Mur <lucymur.dev@gmail.com>
2025-05-18 22:54:05 +0300

    debug

--- tests/playwright/tests/add_and_delete_favorites_e2e.spec.js
+++ tests/playwright/tests/add_and_delete_favorites_e2e.spec.js
@@ -1,10 +1,9 @@
 import { test, expect } from '../fixtures/auth.fixture';
 
 test('test', async ({ page }) => {
-  await page .getByRole('link', { name: 'HEXLING' }).click();
-  await page .getByRole('button', { name: 'Добавить в избранное' }).first().click();
-  await page .getByRole('button', { name: 'user Профиль' }).click();
-  await page .getByRole('link', { name: 'Любимые отели' }).click();
+  await page.getByRole('button', { name: 'Добавить в избранное' }).first().click();
+  await page.getByRole('button', { name: 'user Профиль' }).click();
+  await page.getByRole('link', { name: 'Любимые отели' }).click();
   await page.waitForLoadState('networkidle');
   await page.getByRole('button', { name: 'Удалить из избранного' }).first().click();
   await page.waitForTimeout(500);
--- tests/playwright/tests/support.spec.js
+++ tests/playwright/tests/support.spec.js
@@ -1,12 +1,14 @@
 import { test, expect } from '../fixtures/auth.fixture';
 
 test('test', async ({ page }) => {
-  const uniqueId = `${Date.now()}${Math.floor(Math.random() * 1000)}`
+  const uniqueId = `${Date.now()}${Math.floor(Math.random() * 1000)}`;
+  console.log(uniqueId);
   await page.getByRole('button', { name: 'message' }).click();
   await page.getByRole('textbox', { name: 'Введите сообщение' }).click();
   await page.getByRole('textbox', { name: 'Введите сообщение' }).fill(`test message${uniqueId}`);
   await page.getByRole('button', { name: 'send' }).click();
-  await expect(page.getByText(`test message${uniqueId}`)).toBeVisible({ timeout: 10_000 });;
+  console.log(`Сообщение отправлено test message${uniqueId}`);
+  await expect(page.getByText(`test message${uniqueId}`)).toBeVisible({ timeout: 10_000 });
   await page.getByRole('button', { name: 'Close' }).click();
   await page.getByRole('button', { name: 'user Профиль' }).click();
   await page.getByRole('link', { name: 'Выход' }).click();
